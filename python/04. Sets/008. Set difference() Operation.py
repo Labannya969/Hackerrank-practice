d0e69408acba20dcf8ab9e902d0b66cc373da755
Labannya969 <64127385+Labannya969@users.noreply.github.com>
2021-03-10 23:29:28 +0600

    Create 008. Set difference() Operation.py

---
+++ python/04. Sets/008. Set difference() Operation.py
@@ -0,0 +1,12 @@
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n=int(input())
+set1= set(map(int,input().split()))
+n=int(input())
+set2= set(map(int,input().split()))
+count=0
+sum=0
+set3= set1.intersection(set2)
+res=set1.difference(set3)
+
+print(len(res))
